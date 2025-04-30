bbc1d65f69f1297da3104a80b75612659e841c1a
gloryakinyi <akinyiglory2@gmail.com>
2025-04-30 15:33:10 +0300

    Initial commit

---
+++ app/src/main/java/com/glory/hospitaltracker/navigation/AppNavHost.kt
@@ -0,0 +1,32 @@
+package com.glory.hospitaltracker.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.glory.hospitaltracker.ui.screens.about.AboutScreen
+import com.glory.hospitaltracker.ui.screens.home.HomeScreen
+
+
+@Composable
+fun AppNavHost(
+    modifier: Modifier = Modifier,
+    navController: NavHostController = rememberNavController(),
+    startDestination: String = ROUT_HOME
+) {
+
+    NavHost(
+        navController = navController,
+        startDestination = startDestination,
+        modifier = modifier
+    ) {
+        composable(ROUT_HOME) {
+            HomeScreen(navController)
+        }
+        composable(ROUT_ABOUT) {
+            AboutScreen(navController)
+        }
+    }
+}
---
+++ app/src/main/java/com/glory/hospitaltracker/ui/screens/about/AboutScreen.kt
@@ -0,0 +1,33 @@
+package com.glory.hospitaltracker.ui.screens.about
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+
+
+@Composable
+fun AboutScreen(navController: NavController){
+
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ){
+
+        Text(text = "Welcome to homescreen")
+
+
+    }
+
+}
+
+
+
+@Preview(showBackground = true)
+@Composable
+fun AboutScreenPreview(){
+    AboutScreen(rememberNavController())
+}
